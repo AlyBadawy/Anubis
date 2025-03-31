596c4a08de3a1922800f4451f41fb83ca9795786
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-03-31 12:43:44 -0400

    [Chore] Accounts boilerplate routes (#36)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,20 @@ Rails.application.routes.draw do
         delete "revoke", action: :destroy
       end
     end
+
+    scope "accounts" do
+      # get "current", to: "accounts#current"
+      # get "profile", to: "accounts#show", param: :username
+      # post "login", to: "accounts#login"
+      # post "logout", to: "accounts#logout"
+      # post "register", to: "accounts#register"
+      # post "forgot_password", to: "accounts#forgot_password"
+      # post "reset_password", to: "accounts#reset_password"
+      # post "update_password", to: "accounts#update_password"
+      # post "update_profile", to: "accounts#update_profile"
+      # post "update_avatar", to: "accounts#update_avatar"
+      # post "update_notification_settings", to: "accounts#update_notification_settings"
+    end
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
