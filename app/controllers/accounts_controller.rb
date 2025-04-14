ee1f3e7c07e2f89ccaef8a6186694cf0566b2fbf
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-04-14 00:51:31 -0400

    [Feat] Implement update profile and delete account functionality with authentication (#54)
    * [Feat] Implement update profile and delete account functionality with authentication
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
    
    * Refactor account routes and specs for improved clarity and organization
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -19,6 +19,32 @@ class AccountsController < ApplicationController
     end
   end
 
+  def update_profile
+    @user = Current.user
+    if @user.authenticate(params[:user][:current_password])
+      if @user.update(user_params)
+        render :show, status: :ok, location: @user
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Current password is incorrect" }, status: :unprocessable_entity
+    end
+  end
+
+  def delete_account
+    @user = Current.user
+    if @user.authenticate(params[:user][:current_password])
+      if @user.destroy
+        render json: { message: "Account deleted successfully" }, status: :ok
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Current password is incorrect" }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
--- spec/routing/accounts_routing_spec.rb
+++ spec/routing/accounts_routing_spec.rb
@@ -9,11 +9,19 @@ RSpec.describe AccountsController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(get: "/api/accounts/profile/cool_user").to route_to("accounts#show", username: "cool_user", format: default_format)
+      expect(get: "/api/accounts/account/cool_user").to route_to("accounts#show", username: "cool_user", format: default_format)
     end
 
     it "routes to #register" do
       expect(post: "/api/accounts/register").to route_to("accounts#register", format: default_format)
+    end
+
+    it "routes to #update_profile" do
+      expect(put: "/api/accounts/update").to route_to("accounts#update_profile", format: default_format)
+    end
+
+    it "routes to #delete_account" do
+      expect(delete: "/api/accounts/delete_account").to route_to("accounts#delete_account", format: default_format)
     end
   end
 end
